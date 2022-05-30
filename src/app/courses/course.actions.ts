ea3841bc1675d74fdee350b34815826b87aba1c7
Jeannuel García <jegarcia@altimetrik.com>
2022-05-30 10:39:26 -0500

    Section 5: Ngrx entity, 27. Loading entity data using a router resolver

---
+++ src/app/courses/course.actions.ts
@@ -0,0 +1,11 @@
+import { createAction, props } from "@ngrx/store";
+import { Course } from "./model/course";
+
+export const loadAllCourses = createAction(
+  "[Courses Resolver] Load All courses"
+);
+
+export const allCoursesLoaded = createAction(
+  "[Load Courses Effect] All courses loaded",
+  props<{ courses: Course[] }>()
+);
